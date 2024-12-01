8b2408bccf3eef2466132917a52f49073f6ad7b5
faiaz hasan <98682289+faiaz5415@users.noreply.github.com>
2024-12-01 12:55:09 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CalculatorApp());
+}
+
+class CalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CalculatorScreen(),
+    );
+  }
+}
+
+class CalculatorScreen extends StatefulWidget {
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String _output = "0";
+  String _operand = "";
+  double _num1 = 0;
+  double _num2 = 0;
+
+  void _buttonPressed(String buttonText) {
+    setState(() {
+      if (buttonText == "C") {
+        _output = "0";
+        _num1 = 0;
+        _num2 = 0;
+        _operand = "";
+      } else if (buttonText == "+" ||
+          buttonText == "-" ||
+          buttonText == "*" ||
+          buttonText == "/") {
+        _num1 = double.parse(_output);
+        _operand = buttonText;
+        _output = "0";
+      } else if (buttonText == "=") {
+        _num2 = double.parse(_output);
+        switch (_operand) {
+          case "+":
+            _output = (_num1 + _num2).toString();
+            break;
+          case "-":
+            _output = (_num1 - _num2).toString();
+            break;
+          case "*":
+            _output = (_num1 * _num2).toString();
+            break;
+          case "/":
+            _output = (_num1 / _num2).toString();
+            break;
+        }
+        _num1 = 0;
+        _num2 = 0;
+        _operand = "";
+      } else {
+        if (_output == "0") {
+          _output = buttonText;
+        } else {
+          _output += buttonText;
+        }
+      }
+    });
+  }
+
+  Widget _buildButton(String buttonText) {
+    return Expanded(
+      child: ElevatedButton(
+        onPressed: () => _buttonPressed(buttonText),
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.all(20),
+        ),
+        child: Text(
+          buttonText,
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 24),
+            child: Text(
+              _output,
+              style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: Divider(),
+          ),
+          Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  _buildButton("7"),
+                  _buildButton("8"),
+                  _buildButton("9"),
+                  _buildButton("/"),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  _buildButton("4"),
+                  _buildButton("5"),
+                  _buildButton("6"),
+                  _buildButton("*"),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  _buildButton("1"),
+                  _buildButton("2"),
+                  _buildButton("3"),
+                  _buildButton("-"),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  _buildButton("C"),
+                  _buildButton("0"),
+                  _buildButton("="),
+                  _buildButton("+"),
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
